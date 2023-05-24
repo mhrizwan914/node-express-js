0e976ce72e57e39b11d7fdb705d0e22b9b9e7911
Muhammad Rizwan <mhrizwan914@gmail.com>
2023-05-24 20:23:35 +0500

    Complete LMS API

--- lms-app/backend/controllers/course.js
+++ lms-app/backend/controllers/course.js
@@ -21,14 +21,14 @@ const readAll = async (request, response) => {
 const readById = async (request, response) => {
     try {
         const id = request.params.id;
-        if(!id){
+        if (!id) {
             response.status(404);
             throw new Error("Course Not Found");
         }
         const course_data = await course_model.findById(id);
         response.status(200).send({
             status: 200,
-            message: `Course Get By Id : ${id}  Successfully`,
+            message: `Course Get By Id : ${id} Successfully`,
             data: course_data
         });
     } catch (error) {
@@ -63,22 +63,65 @@ const create = async (request, response) => {
 // @Description : Update Course By Id
 // @Route Get : /api/courses/:id
 // @Access : public
-const update = (request, response) => {
-    response.status(200).send({
-        status: 200,
-        message: `Course Record Updated By Id : ${request.params.id} Successfully`,
-        data: null
-    });
+const update = async (request, response) => {
+    try {
+        const id = request.params.id;
+        if (!id) {
+            response.status(400);
+            throw new Error("Id Must Be Required in URL");
+        } else {
+            const course_update = await course_model.findByIdAndUpdate(id, request.body, { new: true });
+            response.status(200).send({
+                status: 200,
+                message: `Course Updated By Id : ${id}  Successfully`,
+                data: course_update
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
 // @Description : Delete Course By Id
 // @Route Get : /api/courses/:id
 // @Access : public
-const remove = (request, response) => {
-    response.status(200).send({
-        status: 200,
-        message: `Course Deleted Id : ${request.params.id} Successfully`,
-        data: null
-    });
+const remove = async (request, response) => {
+    try {
+        const id = request.params.id;
+        if (!id) {
+            response.status(400);
+            throw new Error("Id Must Be Required in URL");
+        } else {
+            const course_deleted = await course_model.findByIdAndRemove(id);
+            response.status(200).send({
+                status: 200,
+                message: `Course Deleted By Id : ${id}  Successfully`,
+                data: null
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+// @Description : Search Course By Entity
+// @Route Get : /api/courses/search
+// @Access : public
+const search = async (request, response) => {
+    try {
+        const { duration } = request.body;
+        if (!duration) {
+            response.status(404);
+            throw new Error("Search Entity is Required");
+        } else {
+            const course_data = await course_model.find({ duration: duration });
+            response.status(200).send({
+                status: 200,
+                message: "All Courses Search Successfully",
+                data: course_data
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-module.exports = { readAll, readById, create, update, remove };
+module.exports = { readAll, readById, create, update, remove, search };
--- lms-app/backend/routes/course.js
+++ lms-app/backend/routes/course.js
@@ -1,12 +1,13 @@
 // Create Express Server
 const express = require("express");
-const { readAll, readById, create, update, remove } = require("../controllers/course");
+const { readAll, readById, create, update, remove, search } = require("../controllers/course");
 
 // Create Routes
 const router = express.Router();
 
 router.get("/", readAll);
 router.get("/:id", readById);
+router.get("/search", search);
 router.post("/", create);
 router.put("/:id", update);
 router.delete("/:id", remove);
